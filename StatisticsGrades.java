bf1721d327d5d1586f8f1ea26b552fe3b4e89194
Moses Winbood Ayandau <moses.ayandau@amalitech.com>
2024-11-07 16:17:51 +0000

    Added screenshot of the program

--- StatisticsGrades.java
+++ StatisticsGrades.java
@@ -17,6 +17,13 @@ public class StatisticsGrades {
 
     // Convert the input string of grade enter by the user to ArrayList
     static List<Integer> convertStringInputToArrayzList(String input){
+     /**
+      * 1. input : "56 78 87 90 23 12 89 56"
+        2. split it into array of strings
+        3. stringValues: {"56", "78", "87", "90", "23", "12", "89", "56"}
+        4. Converts and adds the array strings into ArrayList of Integers
+
+      */
         List<Integer> grades = new ArrayList<>();
         String[] stringValues = input.split(" ");
        try {
@@ -33,6 +40,11 @@ public class StatisticsGrades {
 
     //Calculate the maximum, minimum and average score of the grades
     static void calculateMaxMinAverageScore(List<Integer> arrayList){
+        /**
+         * 1. Calculates the maximum grade
+         * 2. Calculate the minimum grade
+         * 3. Calculate the average grade
+         */
         for(Integer grade: arrayList){
             if(grade > max){
                 max = grade;
@@ -66,6 +78,9 @@ public class StatisticsGrades {
     }
         //Get the maximum range value that will be used to draw the graph
        static int returnMaxBarToBeDrawn(int[] stats){
+        /**
+         * return: The maximum range of grades
+         */
         for (int count : stats) {
             if (count > maxBar) {
                 maxBar = count;
@@ -75,6 +90,11 @@ public class StatisticsGrades {
        }
        // Draw bar graph patten depending on the maximum range value
        static void drawBarPattern(int[] statRange, int max){
+        /**
+         * 1. Runs a loop starting from the maximum range of grades
+         * 2. Loops through the number of range values
+         * 3. Draws bar if it meets the condition of a bar
+         */
         for (int row = max; row > 0; row--) {
             System.out.print(row+">   \t");
             for (int i = 0; i < statRange.length; i++) {
